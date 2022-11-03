66daae62772a0786aa85629ddf58a81ca58dd87c
Sadman Shawmik <hello@sadmansh.com>
2022-11-03 12:47:02 -0400

    Init empty CRUD functions

---
+++ controller.js
@@ -0,0 +1,29 @@
+class Controller {
+    
+    /* Get all of whatever we are working with */
+    async getAll() {
+        return
+    }
+
+    /* Get individual items of whatever we are working with */
+    async get(id) {
+        return
+    }
+
+    /* Create whatever we are working with */
+    async create(data) {
+        return
+    }
+
+    /* Update whatever we are working with */
+    async update(data) {
+        return
+    }
+
+    /* Delete whatever we are working with */
+    async delete(id) {
+        return 
+    }
+}
+
+module.exports = Controller
