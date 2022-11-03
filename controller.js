fa68b95db41e7c7baa26aaba156b58b12e6b418e
Sadman Shawmik <hello@sadmansh.com>
2022-11-03 13:42:03 -0400

    Add read/write controller functions

--- controller.js
+++ controller.js
@@ -1,29 +1,50 @@
+const fs = require('fs')
+const json = fs.readFileSync('store.json')
+const store = JSON.parse(json)
+
 class Controller {
-    
-    /* Get all of whatever we are working with */
-    async getAll() {
-        return
-    }
+	
+	/* Get all of whatever we are working with */
+	async getAll() {
+		return new Promise((resolve, _) => {
+			resolve(store)
+		})
+	}
 
-    /* Get individual items of whatever we are working with */
-    async get(id) {
-        return
-    }
+	/* Get individual items of whatever we are working with */
+	async get(id) {
+		return new Promise((resolve, reject) => {
+			const item = store.find(i => i.id === parseInt(id))
+			if (item) {
+				resolve(item)
+			} else {
+				reject(`Item with id ${id} not found.`)
+			}
+		})
+	}
 
-    /* Create whatever we are working with */
-    async create(data) {
-        return
-    }
+	/* Create whatever we are working with */
+	async create(data) {
+		return new Promise((resolve, _) => {
+			const item = {
+				id: store.length + 1,
+				...data
+			}
+			store.push(item)
+			fs.writeFileSync('store.json', JSON.stringify(store))
+			resolve(data)
+		})
+	}
 
-    /* Update whatever we are working with */
-    async update(data) {
-        return
-    }
+	/* Update whatever we are working with */
+	async update(item) {
+		
+	}
 
-    /* Delete whatever we are working with */
-    async delete(id) {
-        return 
-    }
+	/* Delete whatever we are working with */
+	async delete(id) {
+		return 
+	}
 }
 
 module.exports = Controller
