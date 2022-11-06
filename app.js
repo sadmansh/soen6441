7bef35a28e759eaafb11c48607a89b81d33a18b2
Sadman Shawmik <hello@sadmansh.com>
2022-11-05 20:14:52 -0400

    Add create table queries

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 require('dotenv/config')
 const http = require('http')
 const PORT = process.env.PORT || 5050
-require('./db')
+const db = require('./db')
+const Recipe = require('./controller')
 
 const server = http.createServer(async (req, res) => {
-    if (req.url === '/api' && req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.write('Hello, SOEN 6441')
-        res.end()
-    } else {
-        res.writeHead(400, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify({ message: `The route ${req.url} does not exist.` }))
-    }
+	if (req.url === '/api/recipes/all' && req.method === 'GET') {
+		res.writeHead(200, { 'Content-Type': 'application/json' })
+		const data = await new Recipe().getAll()
+		res.end(JSON.stringify(data))
+	} else {
+		res.writeHead(400, { 'Content-Type': 'application/json' })
+		res.end(JSON.stringify({ message: `The route ${req.url} does not exist.` }))
+	}
 })
 
 server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}.`)
+	console.log(`Server started on port ${PORT}.`)
 })
--- db.js
+++ db.js
@@ -1,24 +1,72 @@
 const mysql = require('mysql')
 
+const tables = {
+	createRecipeTable: `CREATE TABLE IF NOT EXISTS Recipe(
+		id INT PRIMARY KEY AUTO_INCREMENT,
+		title VARCHAR(500) NOT NULL,
+		summary TEXT,
+		instructions TEXT,
+		image VARCHAR(500),
+		servings INT,
+		time INT
+	)`,
+	createFoodTypeTable: `CREATE TABLE IF NOT EXISTS FoodType (
+		id INT PRIMARY KEY AUTO_INCREMENT,
+		name VARCHAR(500) NOT NULL
+	)`,
+	reateIngredientTable: `CREATE TABLE IF NOT EXISTS Ingredient (
+		id INT PRIMARY KEY AUTO_INCREMENT,
+		name VARCHAR(500) NOT NULL
+	);`,
+	createFoodTypeRelationshipTable: `CREATE TABLE IF NOT EXISTS FoodTypeRelationship (
+		recipe INT NOT NULL,
+		foodType INT NOT NULL,
+		PRIMARY KEY (recipe, foodType),
+		CONSTRAINT constr_recipe_foodtype_fk
+			FOREIGN KEY (recipe) REFERENCES Recipe(id)
+			ON DELETE CASCADE ON UPDATE CASCADE,
+		CONSTRAINT constr_foodType_fk
+			FOREIGN KEY (foodType) REFERENCES FoodType(id)
+			ON DELETE CASCADE ON UPDATE CASCADE
+	)`,
+	createIngredientRelationshipTable: `CREATE TABLE IF NOT EXISTS IngredientRelationship (
+		recipe INT NOT NULL,
+		ingredient INT NOT NULL,
+		PRIMARY KEY (recipe, ingredient),
+		CONSTRAINT constr_recipe_ingredient_fk
+			FOREIGN KEY (recipe) REFERENCES Recipe(id)
+			ON DELETE CASCADE ON UPDATE CASCADE,
+		CONSTRAINT constr_ingredient_fk
+			FOREIGN KEY (ingredient) REFERENCES Ingredient(id)
+			ON DELETE CASCADE ON UPDATE CASCADE
+	)`
+}
+
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME
 })
 
 db.connect((error) => {
-    if (error) {
-        console.error('Cannot connect to database: ', error)
-    } else {
-        console.log('Connected to database.')
-        // Run sample query
-        db.query(`SHOW DATABASES`, (error, result) => {
-            if (error) {
-                console.error('Error executing the query: ', error)
-            } else {
-                console.log('Result: ', result)
-            }
-        })
-    }
+	if (error) {
+		console.error('Cannot connect to database: ', error)
+	} else {
+		console.log('Connected to database.')
+	}
 })
+
+// Create tables
+Object.keys(tables).forEach(query => {
+	db.query(tables[query], (error, result) => {
+		if (error) {
+			console.error('Error executing the query: ', error)
+		} else {
+			console.log(`Table ${query} loaded ✅`)
+		}
+	})
+})
+
+
+module.exports = db
