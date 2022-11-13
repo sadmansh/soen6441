3db60059ef20a4aa467e78c39d68a8d8b515b4e1
Sadman Shawmik <hello@sadmansh.com>
2022-11-12 23:16:33 -0500

    Put main api files in src folder

--- api/test/routes.test.js
+++ api/test/routes.test.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect
 const request = require('supertest')
 const baseUrl = 'localhost:5050'
-require('../app')
+require('../src/app')
 
 describe('GET: Get all recipes', () => {
 	it('Should get the first 100 recipes', (done) => {
