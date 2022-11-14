c3350220c3d875753ad02ba9a6445374f6325246
tarekFerdous <tarek.ferdous@g.bracu.ac.bd>
2022-11-13 22:21:19 -0500

    Remove redundant functions

--- api/src/insertData.js
+++ api/src/insertData.js
@@ -1,134 +1,76 @@
-require('dotenv/config')
-const fs = require('fs')
-const mysql = require('mysql')
-const PoolCluster = require('mysql/lib/PoolCluster')
-const store = fs.readFileSync('store.json')
-let recipes = JSON.parse(store).recipes
+require("dotenv/config");
+const fs = require("fs");
+const mysql = require("mysql");
+const store = fs.readFileSync("store.json");
+let recipes = JSON.parse(store).recipes;
 
 const db = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 // Filter recipes to make sure they contain ingredients and types
-recipes = recipes.filter(recipe => {
-	return recipe.extendedIngredients.length && recipe.dishTypes.length
-})
+recipes = recipes.filter((recipe) => {
+  return recipe.extendedIngredients.length && recipe.dishTypes.length;
+});
 
 recipes.forEach((row, index) => {
-	let stmt = `INSERT INTO Recipe (title, summary, instructions, image, servings, time) VALUES (?, ?, ?, ?, ?, ?)`
-	let query = mysql.format(stmt, [row.title, row.summary, row.instructions, row.image, row.servings, row.readyInMinutes])
-	insertQuery(query)
+  let stmt = `INSERT INTO Recipe (title, summary, instructions, image, servings, time) VALUES (?, ?, ?, ?, ?, ?)`;
+  let query = mysql.format(stmt, [
+    row.title,
+    row.summary,
+    row.instructions,
+    row.image,
+    row.servings,
+    row.readyInMinutes,
+  ]);
+  insertQuery(query);
 
-	// Insert ingredients
-	row.extendedIngredients.forEach(ingredient => {
-		stmt = `
+  // Insert ingredients
+  row.extendedIngredients.forEach((ingredient) => {
+    stmt = `
 			INSERT IGNORE INTO Ingredient (name, amount) VALUES (?, ?);
-		`
-		query = mysql.format(stmt, [ingredient.nameClean, ingredient.original])
-		insertQuery(query)
-		
-		stmt = `
+		`;
+    query = mysql.format(stmt, [ingredient.nameClean, ingredient.original]);
+    insertQuery(query);
+
+    stmt = `
 			INSERT INTO IngredientRelationship (recipe, ingredient) VALUES (
 				(SELECT id FROM Recipe WHERE title = ? LIMIT 1),
 				(SELECT id FROM Ingredient WHERE name = ? LIMIT 1)
 			);
-		`
-		query = mysql.format(stmt, [row.title, ingredient.nameClean])
-		insertQuery(query)
-	})
+		`;
+    query = mysql.format(stmt, [row.title, ingredient.nameClean]);
+    insertQuery(query);
+  });
 
-	// Insert dish type
-	row.dishTypes.forEach(type => {
-		stmt = `
+  // Insert dish type
+  row.dishTypes.forEach((type) => {
+    stmt = `
 			INSERT IGNORE INTO FoodType (name) VALUES (?);
-		`
-		query = mysql.format(stmt, [type])
-		insertQuery(query)
-		stmt = `
+		`;
+    query = mysql.format(stmt, [type]);
+    insertQuery(query);
+    stmt = `
 			INSERT INTO FoodTypeRelationship (recipe, foodType) VALUES (
 					(SELECT id FROM Recipe WHERE title = ? LIMIT 1),
 					(SELECT DISTINCT id FROM FoodType WHERE name = ? LIMIT 1)
 				);
-		`
-		query = mysql.format(stmt, [row.title, type])
-		insertQuery(query)
-	})
-})
+		`;
+    query = mysql.format(stmt, [row.title, type]);
+    insertQuery(query);
+  });
+});
 
 function insertQuery(query) {
-	db.query(query, (err, res) => {
-		if (err) {
-			console.error(err)
-			return
-		} else {
-			console.log('Inserted.')
-		}
-	})
+  db.query(query, (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    } else {
+      console.log("Inserted.");
+    }
+  });
 }
-
-
-
-
-// db.connect((error) => {
-// 	if (error) {
-// 		console.error('Cannot connect to database: ', error)
-// 	} else {
-// 		console.log('Connected to database.')
-// 	}
-// })
-
-// const sqlQuery = (query) => {
-// 	db.query(query, (error, result) => {
-// 		if (error) {
-// 			console.error('Error executing the query: ', error)
-// 		} else {
-// 			console.log(result)
-// 		}
-// 	})
-// }
-
-recipes.slice(0, 1).forEach(row => {
-	// Insert recipe
-	// let stmt = `INSERT INTO Recipe VALUES (?, "${row.title}", "${row.summary}", "${row.instructions}", "${row.image}", ${row.servings}, ${row.readyInMinutes});`
-	// sqlQuery(stmt)
-
-	// Insert recipe ingredient
-	// row.extendedIngredients.forEach(ingredient => {
-	// 	stmt = `
-	// 		INSERT IGNORE INTO Ingredient (name, withAmount)
-	// 			VALUES ("${ingredient.nameClean}", "${ingredient.original}");
-	// 	`
-	// 	sqlQuery(stmt)
-
-	// 	stmt = `
-	// 		INSERT INTO IngredientRelationship (recipe, ingredient)
-	// 			VALUES (
-	// 				(SELECT id FROM Recipe WHERE title = "${row.title}"),
-	// 				(SELECT id FROM Ingredient WHERE name = "${ingredient.nameClean}")
-	// 			);
-	// 	`
-	// 	sqlQuery(stmt)
-	// })
-
-	// // Insert dish type
-	// row.dishTypes.forEach(type => {
-	// 	stmt = `
-    //         INSERT IGNORE INTO FoodType (name)
-    //             VALUES (${type});
-    //     `
-	// 	sqlQuery(stmt)
-
-    //     stmt = `
-    //         INSERT INTO FoodTypeRelationship (recipe, foodType)
-    //             VALUES (
-    //                 (SELECT id FROM Recipe WHERE title = "${row.title}"),
-    //                 (SELECT id FROM FoodType WHERE name = "${type}")
-    //             );
-	// 	`
-	// 	sqlQuery(stmt)
-	// })
-
-})
